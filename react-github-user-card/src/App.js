4af5d18637e3db56c14d14cdc42de13559f0ba27
wesley.moody <wgm3190@gmail.com>
2020-01-14 16:05:13 -0600

    MVP done some styling

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UserCard from "./components/UserCard";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      login: "",
+      avatar: "",
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/wesley-moody").then(response => {
+      // console.log (response.data.name)
+
+      this.setState({
+        name: response.data.name,
+        email: response.data.email,
+        login: response.data.login,
+        avatar: response.data.avatar_url
+      });
+    });
+    axios
+      .get("https://api.github.com/users/wesley-moody/followers")
+      .then(response => {
+        console.log(response);
+
+        this.setState({
+          followers: response.data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.followers.map(followme => {
+          return (
+            <div className='followers'>
+              <img src={followme.avatar_url} />
+              <div className='followersinfo'>
+                <p>{followme.name}</p>
+                <p>USERNAME: {followme.login}</p>
+                <p> Click below to see my work on github! </p>
+                <button>{followme.html_url}</button>
+              </div>
+            </div>
+          );
+        })}
+
+        {console.log(this.state)}
+        <UserCard
+          name={this.state.name}
+          email={this.state.email}
+          login={this.state.login}
+          avatar={this.state.avatar}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
